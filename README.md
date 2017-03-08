d5e6895e807e16213375f4b8daf4fd88e10377e3
nemupm <nemupm@gmail.com>
2017-03-08 22:26:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Programming Contest Template
+
+---
+
+# Usage
+
+```
+g++ answer.cpp -o answer.exe # for submit
+g++ answer.cpp -o answer.exe -DLOCAL # for debug
+./answer.exe < example1.in
+./answer.exe < example2.in
+```
+
---
+++ skeleton/answer.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <cmath>
+
+#define REP(i,x) for(int i=0;i<(int)(x);i++)
+#define REPS(i,x) for(int i=1;i<=(int)(x);i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define all(x) (x).begin(),(x).end()
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define eprintf(...) 42
+#endif
+
+typedef long long ll;
+
+using namespace std;
+
+const int MAX_N = 100000;
+int N;
+int A[MAX_N];
+
+int solve() {
+    return 0;
+}
+
+int main() {
+    // to shorten execution time for iostream
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+    REP(i,N) cin >> A[i];
+    cout << solve() << "\n";
+    return 0;
+}
+
