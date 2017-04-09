2300e5555d86378d3d7109499fd33bab78f1ffd4
nemupm <nemupm@gmail.com>
2017-04-09 22:30:10 +0900

    updated

--- skeleton/answer.cpp
+++ skeleton/answer.cpp
@@ -12,16 +12,25 @@
 #include <functional>
 #include <cmath>
 
-#define REP(i,x) for(int i=0;i<(int)(x);i++)
-#define REPS(i,x) for(int i=1;i<=(int)(x);i++)
-#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
-#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define FOR(i,x,y) for(int i=(x);i<(y);i++)
+#define RFOR(i,x,y) for(int i=(y)-1;i>=(x);i--)
+#define REP(i,x) for(int i=0;i<(x);i++)
+#define RREP(i,x) for(int i=(x)-1;i>=0;i--)
 #define all(x) (x).begin(),(x).end()
+// #define int long long
+
+// conversion
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
 
 #ifdef LOCAL
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#define dump(x)  cerr << #x << " = " << (x) << endl
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl
 #else
 #define eprintf(...) 42
+#define dump(x) 42
+#define debug(x) 42
 #endif
 
 typedef long long ll;
@@ -36,7 +45,7 @@ int solve() {
     return 0;
 }
 
-int main() {
+signed main() {
     // to shorten execution time for iostream
     cin.tie(0);
     ios::sync_with_stdio(false);
